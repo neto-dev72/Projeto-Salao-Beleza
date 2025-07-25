0c2f4f3349a268817304e649d920df339e66c560
bernardo antonio <aldairnew11@gmail.com>
2025-07-25 12:23:37 +0100

    Descrição das mudanças que você fez

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,19 +2,13 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
+  process.env.DB_NAME,      // 'salao'
+  process.env.DB_USER,      // 'root'
+  process.env.DB_PASS,      // '12345678'
   {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5432,
-    dialect: 'postgres',  // trocar para postgres
+    host: process.env.DB_HOST,  // 'localhost'
+    dialect: 'mysql',
     logging: false,
-    dialectOptions: {
-      ssl: {
-        rejectUnauthorized: false  // necessário para conexão com Render
-      }
-    }
   }
 );
 
